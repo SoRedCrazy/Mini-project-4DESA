02916e3511050ec3d866b074114aa83e935a94a0
Julien boisgard <julien.boisgard37@gmail.com>
2024-01-25 11:30:34 +0100

    try db

--- init.py
+++ init.py
@@ -31,9 +31,5 @@ if __name__ == "__main__":
 
 
 def get_conn():
-    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
-    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
-    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
-    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
-    conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+    conn = pyodbc.connect(connection_string)
     return conn
