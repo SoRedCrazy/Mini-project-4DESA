d6e548f774c269a3246b63828d48665b36897b30
Julien boisgard <julien.boisgard37@gmail.com>
2024-01-26 14:03:54 +0100

    add search et comment

--- init.py
+++ init.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from azure.storage.blob import BlockBlobService
+
 import pyodbc, struct ,os
 import json
 
@@ -239,10 +239,120 @@ def delete_post():
     return jsonify({"State": 400})
 
 
+@app.route('/comment', methods=['GET'])
+@jwt_required()
+def get_comment():
+    name = request.args.get('pseudo')
+    records=[]
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+
+        if name == None :
+            cursor.execute("SELECT * FROM comment;")
+        elif not is_private(name):
+            cursor.execute("SELECT * FROM comment users='"+name+"';")
+
+        records = cursor.fetchall()
+    except Exception as e:
+        print(e)
+
+    list_comment=[]
+    for r in records:
+        temp= {
+        "id" : r.id,
+        "post_id" : r.post,
+        "pictures_id" : r.pictures,
+        "video_id": r.video,
+        "comment": r.text_comment,
+        "author": r.users
+        }
+        list_comment.append(temp)
+    
+    return jsonify(list_comment)
 
 
+@app.route('/comment', methods=['POST'])
+@jwt_required()
+def post_comment():
+    comment = str(request.json.get("text", None))
+    type_post = str(request.json.get("type", None))
+    uid = str(request.json.get("id", None))
+
+    current_user_id = get_jwt_identity()
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        if type_post == "video":
+            cursor.execute("""
+                INSERT INTO comment (users, text_comment, video)
+                VALUES ('"""+current_user_id+"""','"""+comment+"""','"""+uid+"""');
+            """)
+        elif type_post == "pictures":
+            cursor.execute("""
+                INSERT INTO comment (users, text_comment, pictures)
+                VALUES ('"""+current_user_id+"""','"""+comment+"""','"""+uid+"""');
+            """)
+        elif type_post == "post":
+            cursor.execute("""
+                INSERT INTO comment (users, text_comment, post)
+                VALUES ('"""+current_user_id+"""','"""+comment+"""','"""+uid+"""');
+            """)
+        cursor.commit()
+        return jsonify({"State": 201})
+    except Exception as e:
+        print(e)
+
+    return jsonify({"State": 400})
+
+@app.route('/comment', methods=['PUT'])
+@jwt_required()
+def put_comment():
+    uid = request.json.get("id", None)
+    comment = request.json.get("text", None)
+
+    current_user_id = get_jwt_identity()
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE comment
+            SET text_comment = '"""+comment+"""'
+            WHERE users = '"""+current_user_id+"""' and id='"""+uid+"""'
+        """)
+        cursor.commit()
+        return jsonify({"State": 201})
+    except Exception as e:
+        print(e)
+
+    return jsonify({"State": 400})
 
 
+@app.route('/comment', methods=['DELETE'])
+@jwt_required()
+def delete_comment():
+    uid = request.json.get("id", None)
+
+    current_user_id = get_jwt_identity()
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
+        if is_admin(current_user_id):
+            cursor.execute("""
+                DELETE FROM comment
+                WHERE id='"""+uid+"""'
+            """)
+        else:
+            cursor.execute("""
+                DELETE FROM comment
+                WHERE users='"""+current_user_id+"""' and id='"""+uid+"""'
+            """)
+        cursor.commit()
+        return jsonify({"State": 201})
+    except Exception as e:
+        print(e)
+
+    return jsonify({"State": 400})
 
 
 @app.route('/search', methods=['GET'])
@@ -250,12 +360,13 @@ def delete_post():
 def search():
     results={"post":[], "pictures":[], "movies":[]}
     pseudo = request.json.get("pseudo", None)
+
     if not is_private(pseudo):
         records=[]
         try:
             conn = get_conn()
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM post users='"+pseudo+"';")
+            cursor.execute("SELECT * FROM post WHERE users='"+pseudo+"';")
             cursor.commit()
             records = cursor.fetchall()
         except Exception as e:
@@ -268,11 +379,42 @@ def search():
             "author": r.users
             }
             results["post"].append(temp)
+
+        try:
+            conn = get_conn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM pictures WHERE users='"+pseudo+"';")
+            cursor.commit()
+            records = cursor.fetchall()
+        except Exception as e:
+            print(e)
+
+        for r in records:
+            temp= {
+            "id" : r.id,
+            "name": r.name,
+            "author": r.users
+            }
+            results["pictures"].append(temp)
+        
+        try:
+            conn = get_conn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM video WHERE users='"+pseudo+"';")
+            cursor.commit()
+            records = cursor.fetchall()
+        except Exception as e:
+            print(e)
+
+        for r in records:
+            temp= {
+            "id" : r.id,
+            "name": r.name,
+            "author": r.users
+            }
+            results["movies"].append(temp)
     
     return jsonify(results)
-
-
-
 
 
 
@@ -353,7 +495,7 @@ def initialisation():
                 pictures int,
                 video int,
                 id int IDENTITY(1,1) PRIMARY KEY,
-                commant VARCHAR(255) NOT NULL,
+                text_comment VARCHAR(255) NOT NULL,
                 FOREIGN KEY (users) REFERENCES users(pseudo),
                 FOREIGN KEY (post) REFERENCES post(id),
                 FOREIGN KEY (pictures) REFERENCES pictures(id),
@@ -379,7 +521,7 @@ def is_private(name):
     try:
         conn = get_conn()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM user pseudo='"+name+"';")
+        cursor.execute("SELECT * FROM users WHERE pseudo='"+name+"';")
         records = cursor.fetchall()
     except Exception as e:
         print(e)
@@ -391,12 +533,12 @@ def is_admin(name):
     try:
         conn = get_conn()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM user pseudo='"+name+"';")
+        cursor.execute("""SELECT * FROM users WHERE pseudo='"""+name+"""';""")
         records = cursor.fetchall()
     except Exception as e:
         print(e)
 
-    return records[0].pris_admin
+    return records[0].is_admin
 
 
 def get_conn():
